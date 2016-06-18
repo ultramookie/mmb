13ee933e805e892c9840331c3673f19fda0ebd13
Mookie Kong <mookie@outlook.com>
2016-06-18 17:06:46 +0000

    adding functionality

--- mmb.py
+++ mmb.py
@@ -2,26 +2,44 @@
 
 import json
 import markdown
+import optparse
+import glob
 import pprint
 
-def read_config():
-  with open('config.json') as config_file:    
+# Get me my config values!
+def read_config(input_dir):
+  config_location = input_dir + '/config.json'
+  with open(config_location) as config_file:    
     config = json.load(config_file)
   return config
 
+# What am I trying to translate?
 def read_body():
   mdfile = open('test.md').read()
   html = markdown.markdown(mdfile)
   return html
 
+# Where is the stuff?
+def get_input_dir():
+  parser = optparse.OptionParser()
+  parser.add_option('-i', action="store", help="Where are your input files located?", dest="input_dir", default="./")
+  (options, args) = parser.parse_args()
+  return options.input_dir
+
+# Do the doing
+def process_entries(input_dir,config):
+  entry_files = glob.glob(input_dir + '/*.md')
+  done_files = glob.glob(input_dir + '/*.done')
+  pprint.pprint(entry_files)
+  pprint.pprint(done_files)
+
+# Go Speed Go
 def run():
-  config = read_config()
+  input_dir = get_input_dir()
+  config = read_config(input_dir)
   pprint.pprint(config)
   for k in config:
     print "%s: %s" % (k,config[k])
-  body = read_body()
-  print(body)
+  process_entries(input_dir,config)
 
 run()
-
-
