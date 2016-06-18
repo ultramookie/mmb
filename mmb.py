c6bdff9b2bd15fd54196be72e47e2f83ab756037
Mookie Kong <mookie@outlook.com>
2016-06-18 21:34:19 +0000

    convert footer to jinja. jinja rendering complete

--- mmb.py
+++ mmb.py
@@ -42,34 +42,36 @@ def get_title(metadata):
   title = ' '.join(metadata[3:])
   return title
 
-def create_header(header_template,metadata):
+def render_jinja(incoming_template,metadata,config):
   date = get_date(metadata)
-  title = get_title(metadata)
-  print date
-  print title
-  print header_template
+  title = get_title(metadata).title()
+  blog_name = config['blog_name']
+  meta_keywords = config['meta_keywords']
+  base_url = config['base_url']
+  url_location = config['url_location']
+  template = jinja2.Template(incoming_template)
+  rendered_jinja = template.render(title=title,date=date,blog_name=blog_name,meta_keywords=meta_keywords,base_url=base_url,url_location=url_location)
+  return rendered_jinja
 
 # Do the doing
 def process_entries(input_dir,config):
   header_template = open(config['header_file']).read()
-  footer = open(config['footer_file']).read()
+  footer_template = open(config['footer_file']).read()
   entry_files = sorted(glob.glob(input_dir + '/*.md'))
   for entry_file in entry_files:
     base_filename = os.path.splitext(entry_file)[0]
     metadata = get_meta_data(base_filename)
     if not os.path.isfile(base_filename + '.done'):
-      header_html = create_header(header_template,metadata)
+      header_html = render_jinja(header_template,metadata,config)
+      footer_html = render_jinja(footer_template,metadata,config)
       body_html = read_body(entry_file)
-      html_doc = body_html + footer
+      html_doc = header_html + body_html + footer_html
       print html_doc
 
 # Go Speed Go
 def run():
   input_dir = get_input_dir()
   config = read_config(input_dir)
-  pprint.pprint(config)
-  for k in config:
-    print "%s: %s" % (k,config[k])
   process_entries(input_dir,config)
 
 run()
